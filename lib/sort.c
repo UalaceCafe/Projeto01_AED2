2017b93b696050f487c0263d7504c419d0f36950
Ualace Henrique Santos Café <ualacehenrique@hotmail.com>
2022-07-25 23:36:43 -0300

    update sort.c with more performant code

--- lib/sort.c
+++ lib/sort.c
@@ -1,56 +1,47 @@
 #include <string.h>
 #include "../include/sort.h"
 
-<<<<<<< HEAD
 long int cont_comp;
 long int cont_swap;
 
-void quick_sort(info * array, int left, int right)
-=======
 void quick_sort(info *array, int left, int right)
->>>>>>> d707c01e945c359c504d5c0a95a97482991513ec
 {
-    int pivo = left, j;
+    int i, j;
+    char * x;
     char tempName[100];
     char tempPhone[20];
 
-    for (int i = left + 1; i <= right; i++)
+    i = left;
+    j = right;
+    x = array[(left + right) / 2].name;
+
+    do
     {
-<<<<<<< HEAD
-        while ((strcmp(array[i].name, x) < 0) && (i < right)) {
+        while ((strcmp(array[i].name, x) < 0) && (i < right))
+        {
             cont_comp++;
             i++;
         }
 
-        while ((strcmp(array[j].name, x) > 0) && (j > left)) {
+        while ((strcmp(array[i].name, x) > 0) && (j > left))
+        {
             cont_comp++;
             j--;
         }
 
         if (i <= j)
-=======
-        j = i;
-
-        if (strcmp(array[j].name, array[pivo].name) < 0)
->>>>>>> d707c01e945c359c504d5c0a95a97482991513ec
         {
-            strcpy(tempName, array[j].name);
-            strcpy(tempPhone, array[j].phone);
-
-            while (j > pivo)
-            {
-                strcpy(array[j].name, array[j - 1].name);
-                strcpy(array[j].phone, array[j - 1].phone);
-                j--;
-            }
-
+            strcpy(tempName, array[i].name);
+            strcpy(tempPhone, array[i].phone);
+            strcpy(array[i].name, array[j].name);
+            strcpy(array[i].phone, array[j].phone);
             strcpy(array[j].name, tempName);
             strcpy(array[j].phone, tempPhone);
-<<<<<<< HEAD
             cont_swap += 2;
             i++;
             j--;
         }
+
     } while (i <= j);
 
     if (left < j)
@@ -60,7 +51,7 @@ void quick_sort(info *array, int left, int right)
         quick_sort(array, i, right);
 }
 
-void insertion_sort(info * array, int size)
+void insertion_sort(info *array, int size)
 {
     int i, j;
     char tempName[100];
@@ -82,40 +73,4 @@ void insertion_sort(info * array, int size)
             cont_swap += 2;
         }
     }
-=======
-            pivo++;
-        }
-    }
-
-    if (pivo - 1 >= left)
-        quick_sort(array, left, pivo - 1);
-
-    if (pivo + 1 <= right)
-        quick_sort(array, pivo + 1, right);
-}
-
-void insertion_sort(info *array, int size)
-{
-    int i, j;
-    char tempName[100];
-    char tempPhone[20];
-
-    for (i = 1; i <= size - 1; i++)
-    {
-        strcpy(tempName, array[i].name);
-        strcpy(tempPhone, array[i].phone);
-
-        j = i - 1;
-
-        while (j >= 0 && strcmp(tempName, array[j].name) < 0)
-        {
-            strcpy(array[j + 1].name, array[j].name);
-            strcpy(array[j + 1].phone, array[j].phone);
-            j--;
-        }
-
-        strcpy(array[j + 1].name, tempName);
-        strcpy(array[j + 1].phone, tempPhone);
-    }
->>>>>>> d707c01e945c359c504d5c0a95a97482991513ec
 }
